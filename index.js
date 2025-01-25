0607b8bede1f95c148f6ac121c18089fd57e7e1b
Koiralapradeep <np03cs4a220433@heraldcollege.edu.np>
2025-01-25 14:48:12 +0545

    working on employee dashboard

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/DB.js";
 import employee from "./routes/employee.js";
 import department from "./routes/department.js";
-import authRoutes from "./routes/auth.js"; // Import the auth.js routes
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 connectDB();
@@ -12,8 +12,12 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', authRoutes); // Connect the auth routes
-app.use("/public/uploads", express.static("public/uploads")); // Serve uploaded files
+
+// Serve the uploaded images from /public/uploads
+app.use("/public/uploads", express.static("public/uploads"));
+
+// Route handlers
+app.use("/api/auth", authRoutes);
 app.use("/api/employee", employee);
 app.use("/api/departments", department);
 
