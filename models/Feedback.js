4ef115dbd9c7f2c4060936eda4bff496424a7464
Koiralapradeep <np03cs4a220433@heraldcollege.edu.np>
2025-02-14 23:11:01 +0545

    Save local changes before merging

--- models/Feedback.js
+++ models/Feedback.js
@@ -6,6 +6,9 @@ const feedbackSchema = new mongoose.Schema(
     accomplishments: { type: String, required: true },
     challenges: { type: String, required: true },
     suggestions: { type: String, required: true },
+
+    makePrivate: { type: Boolean, default: false },       // New field: if true, employee info is hidden in Manager view
+    saveToDashboard: { type: Boolean, default: false },     // New field: if true, feedback is permanently saved on Employee Dashboard
   },
   { timestamps: true }
 );
